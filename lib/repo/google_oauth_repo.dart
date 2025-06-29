39fd311287a79b5bae04b491e38730b9efca55a2
Ahmed Wael <ahmedwael99104@gmail.com>
2025-06-29 22:02:36 +0300

    define windows and android web view controller as repo paramters

--- lib/repo/google_oauth_repo.dart
+++ lib/repo/google_oauth_repo.dart
@@ -1,10 +1,19 @@
 import 'package:google_oauth2/models/google_oauth_model.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_windows/webview_windows.dart';
 
 abstract interface class GoogleOAuthRepo {
   Future<void> authWithGoogle({required GoogleOauthModel model});
 }
 
 class GoogleOAuthRrepoImpl implements GoogleOAuthRepo {
+  final WebviewController? controller;
+  final WebViewController? mobileController;
+
+  GoogleOAuthRrepoImpl({
+    required this.controller,
+    required this.mobileController,
+  });
   @override
   Future<void> authWithGoogle({required GoogleOauthModel model}) async {
     throw UnimplementedError();
