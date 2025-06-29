3c00165b6996ed9f76c82d27571f904db6fade6c
Ahmed Wael <ahmedwael99104@gmail.com>
2025-06-29 22:17:31 +0300

    add google oauthrepo impl

--- lib/repo/google_oauth_repo.dart
+++ lib/repo/google_oauth_repo.dart
@@ -1,6 +1,15 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_oauth2/models/google_auth_callback.dart';
 import 'package:google_oauth2/models/google_oauth_model.dart';
+import 'package:google_oauth2/utils/auth_req_sec_params_generator.dart';
+import 'package:google_oauth2/utils/google_auth_listener.dart';
+import 'package:google_oauth2/utils/google_auth_url.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_windows/webview_windows.dart';
+
+import '../models/google_auth_url_paarmeters.dart';
 
 abstract interface class GoogleOAuthRepo {
   Future<void> authWithGoogle({required GoogleOauthModel model});
@@ -16,6 +25,63 @@ class GoogleOAuthRrepoImpl implements GoogleOAuthRepo {
   });
   @override
   Future<void> authWithGoogle({required GoogleOauthModel model}) async {
-    throw UnimplementedError();
+    final secParams = AuthReqSecParamsGen.genAuthReqSecParams();
+    final authorizationRequestUri = GoogleAuthUrl.getGoogleAuthUrl(
+      parameters: GoogleAuthUrlParameters(
+        clientId: model.setup.clientId,
+        redirectUri: model.setup.redirectUri,
+        responseType: "code",
+        scopes: model.setup.scopes,
+        codeChallenge: secParams.codeChallange,
+        codeChallengeMethod: "S256",
+        state: secParams.state,
+        loginHint: model.setup.loginHint,
+      ),
+    );
+    if (Platform.isWindows) {
+      await controller?.initialize();
+      await controller?.setBackgroundColor(Colors.transparent);
+      controller?.url.listen((url) async {
+         await GoogleAuthCallbackHandler.listenForAuthCallback(
+          googleAuthCallback: GoogleAuthCallback(
+            url: url,
+            clientId: model.setup.clientId,
+            clientSecret: model.setup.clientSecret,
+            redirectUri: model.setup.redirectUri,
+            codeVerifier: secParams.codeVerifier,
+            onSuccess: model.setup.onSuccess,
+            onError: model.setup.onError,
+            onLoading: model.onLoading,
+          ),
+        );
+      });
+      await controller?.loadUrl(authorizationRequestUri);
+    }
+    if (Platform.isAndroid) {
+      await mobileController?.setJavaScriptMode(JavaScriptMode.unrestricted);
+      await mobileController?.setUserAgent(
+        "Mozilla/5.0(Linux; Android 10; Mobile) AppleWebKit/537.36 (KHTML , like Gecko) Chrome/86.0.4240.198 Mobile Safari/537.36",
+      );
+      await mobileController?.setNavigationDelegate(
+        NavigationDelegate(
+          onPageFinished: (url) async {
+            await GoogleAuthCallbackHandler.listenForAuthCallback(
+              googleAuthCallback: GoogleAuthCallback(
+                url: url,
+                clientId: model.setup.clientId,
+                clientSecret: model.setup.clientSecret,
+                redirectUri: model.setup.redirectUri,
+                codeVerifier: secParams.codeVerifier,
+                onSuccess: model.setup.onSuccess,
+                onError: model.setup.onError,
+                onLoading: model.onLoading,
+              ),
+            );
+          },
+        ),
+      );
+      await mobileController?.loadRequest(Uri.parse(authorizationRequestUri));
+    }
+    model.onInitalized();
   }
 }
