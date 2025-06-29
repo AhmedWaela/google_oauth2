9ba266d75b68d7bcf9fb1e4873818e87e2acf1e2
Ahmed Wael <ahmedwael99104@gmail.com>
2025-06-29 22:14:33 +0300

    create google auth callback model

---
+++ lib/models/google_auth_callback.dart
@@ -0,0 +1,25 @@
+
+import 'package:google_oauth2/errors/google_auth_failure.dart';
+import '../../models/google_tokens.dart';
+
+class GoogleAuthCallback {
+  final String url;
+  final String clientId;
+  final String clientSecret;
+  final String redirectUri;
+  final String codeVerifier;
+  final Function(GoogleTokens tokens)? onSuccess;
+  final Function(GoogleAuthFailure)? onError;
+  final Function() onLoading;
+
+  GoogleAuthCallback({
+    required this.url,
+    required this.clientId,
+    required this.clientSecret,
+    required this.redirectUri,
+    required this.codeVerifier,
+    required this.onSuccess,
+    required this.onError,
+    required this.onLoading,
+  });
+}
